ff364f2e4ad9ccaf2b1a50f29aec6e147d2e0f0a
Raphael <raphael@joindiaspora.com>
2011-01-20 19:05:00 -0800

    Starting work on an autocomplete feature

--- app/models/person.rb
+++ app/models/person.rb
@@ -148,15 +148,7 @@ class Person < ActiveRecord::Base
   end
 
   def as_json(opts={})
-    {
-      :person => {
-        :id           => self.guid,
-        :name         => self.name,
-        :url          => self.url,
-        :exported_key => exported_key,
-        :diaspora_handle => self.diaspora_handle
-      }
-    }
+   {:id => self.guid, :label => self.name, :value => self.name}
   end
 
   def self.from_post_comment_hash(hash)
